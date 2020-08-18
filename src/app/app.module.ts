5b7a00e37339adf70ae3e69f95c775510f58a069
troy <hanson.troy12@gmail.com>
2020-08-18 13:55:06 -0400

    second commit for todo lab

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { TodoComponent } from './todo/todo.component';
 
+//Decarator
 @NgModule({
-  declarations: [
-    AppComponent,
-    TodoComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, TodoComponent],
+  imports: [BrowserModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Todo } from '../interfaces/todo';
+import { NgForm } from '@angular/forms';
 
+//Decarator
 @Component({
   selector: 'app-todo',
   templateUrl: './todo.component.html',
@@ -26,8 +28,53 @@ export class TodoComponent implements OnInit {
     },
   ];
 
+  searchTerm: string;
+
+  showIndex: number;
+
   //methods below here
   constructor() {}
+
+  setShowIndex(index: number) {
+    this.showIndex = index;
+  }
+
+  resetShowIndex() {
+    this.showIndex = undefined;
+  }
+
+  addTask(form: NgForm) {
+    console.log(form);
+    let newTodo: Todo = {
+      task: form.value.todo,
+      completed: false,
+    };
+    this.todos.push(newTodo);
+    form.reset();
+  }
+
+  removeTask(index: number) {
+    this.todos.splice(index, 1);
+  }
+
+  completeTask(index: number) {
+    this.todos[index].completed = true;
+  }
+
+  setSearchTerm(form: NgForm) {
+    this.searchTerm = form.value.searchTerm.trim().toLowerCase();
+  }
+
+  myFilterMethod() {
+    if (!this.searchTerm) {
+      return this.todos;
+    } else {
+      return this.todos.filter((objective) => {
+        let currentTodo = objective.task.toLowerCase().trim();
+        return currentTodo.includes(this.searchTerm);
+      });
+    }
+  }
 
   ngOnInit(): void {}
 }
